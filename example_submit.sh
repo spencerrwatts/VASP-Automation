c5f505048a9fcf54845cc4ba8008740ff6837b73
Spencer Watts <swatt15@lsu.edu>
2026-07-10 13:47:59 -0500

    Update and rename submit.sh to example_submit.sh

--- example_submit.sh
+++ example_submit.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-#SBATCH -p gpu
+#SBATCH -p workq
 #SBATCH -N 1
 #SBATCH -n 64
 #SBATCH -c 1
 #SBATCH -t 72:00:00
-#SBATCH -A hpc_te_mater_1
-#SBATCH -J EuAl2Cl8
+#SBATCH -A 'storage allocation'
+#SBATCH -J VASP_automation
 #SBATCH -o vasp.out
 #SBATCH -e vasp.err
-#SBATCH --mail-user=swatt15@lsu.edu
 
 module purge
 module load intel-mpi/2021.5.1
@@ -19,6 +18,6 @@ export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 echo $SLURM_SUBMIT_DIR
 cd   $SLURM_SUBMIT_DIR
 
-time srun -N1 -n64 /project/svyatium/vasp/vasp.6.4.1/bin/vasp_std > vasp_run.out
+time srun -N1 -n64 /YOUR/VASP/EXECUTABLE/LOCATION/vasp_std > vasp_run.out
 
 
